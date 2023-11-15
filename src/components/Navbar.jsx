e9ac180cfdc6442333b3bd353d7a59e8f00cb408
Milan-Sharma-449 <milansharma449@gmail.com>
2023-11-15 12:52:31 +0530

    added new functionalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,9 @@ const Navbar = () => {
         <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
           Projects
         </NavLink>
+        <NavLink to='https://milansharma.me/portfolio'>
+          Portfolio
+        </NavLink>
       </nav>
     </header>
   );
