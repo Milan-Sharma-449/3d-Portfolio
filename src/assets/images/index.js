3d46e9a8aa97f51dbbde291a4f9b406ef1c35f8e
Milan-Sharma-449 <milansharma449@gmail.com>
2023-11-15 12:32:25 +0530

    build 3d portfolio

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './hrc_highako.png'
+import tesla from './tesla.png'
+import logo from './ms2.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,239 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    x,
+    instagram,
+    facebook
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Front End Developer",
+        company_name: "HighRadius Corporate",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "Dec 2021 - June 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Data Analyst",
+        company_name: "HighRadius Corporate",
+        icon: starbucks,
+        iconBg: "#fbc3bc",
+        date: "July 2021 - Dec 2021",
+        points: [
+            "Worked on MySql yog Server for queries fetching and making.",
+            "Worked as a Analyst under the POD Lead for dashboard queries.",
+            "Implemented many useful tools such as Jira, Mongo Atlas, Snowflake, Bouncer etc.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Analyst Trainee Intern",
+        company_name: "HighRadius Corporate",
+        icon: starbucks,
+        iconBg: "#b7e4c7",
+        date: "June 2021 - July 2021",
+        points: [
+            "Worked as a intern in the product based and treasury based firm.",
+            "Build a project for the Company to make a AI Fintech B2B Management System Application.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Software Used: VS Code, MySql Server, Eclipse and Language Used: Html, Css, JavaScript, Java, Sql etc.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Milan-Sharma-449',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/milan-sharma-3b9985166/',
+    },
+    {
+        name: 'Instagram',
+        iconUrl: instagram,
+        link: 'https://www.instagram.com/milansharma_449/',
+    },
+    {
+        name: 'Facebook',
+        iconUrl: facebook,
+        link: 'https://www.facebook.com/milan.sharma.980315/',
+    },
+    {
+        name: 'X',
+        iconUrl: x,
+        link: 'https://twitter.com/milansharma449',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: car,
+        theme: 'btn-back-red',
+        name: 'Personal Portfolio',
+        description: 'Developed a fully functional Personal Portfolio to describe me and showing my skills to the Developers and to get me hired in a top MNC. Based on ReactJs',
+        link: 'https://github.com/Milan-Sharma-449/Portfolio-react',
+        llink: 'https://milansharma.me/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: "Gold's Gym",
+        description: 'Developed and build a fully functional website which shows the exercises and gives us the report of our body to be fully fit. Build using ReactJs and uses API for fetching exercises.',
+        link: 'https://github.com/Milan-Sharma-449/gold-s-gym',
+        llink: "https://milansharma.me/Gold's-Gym/",
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-blue',
+        name: 'Box-Office',
+        description: 'Box-office is a react based website which used to show the description related to the searched Show or Actor such as (About, release date, aired if any, no. of season or episodes, cast, dates) and also you can make your own loved list by starring them.',
+        link: 'https://github.com/Milan-Sharma-449/box-office',
+        llink: 'https://milansharma.me/boxoffice',
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-pink',
+        name: 'tMovies',
+        description: 'Watching free movies online is a convenient and frugal way to see the films you love right from the comfort of your own home. Yes, this is the site where you can get movies info "for free" but the ones listed below, although info also you can watch trailers and, are clean from viruses and completely legal to use.',
+        link: 'https://github.com/Milan-Sharma-449/tMovies',
+        llink: 'https://milansharma.me/tmovies/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Sakila Project(Hrc)',
+        description: 'Full Stack ExtJs Dynamic Java-EE Film Inventory Project with CURD Operations implemented using MySQL Server, Hibernate, Spring MVC with Struts2.',
+        link: 'https://github.com/Milan-Sharma-449/Hrc-Training-Sakila-',
+        llink: 'https://github.com/Milan-Sharma-449/Hrc-Training-Sakila-',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Sinit Chat',
+        description: 'Sinit Chat allows you to communicate with your customers through chat. It enables you to send and receive messages. Chatting apps make it easier, simpler and faster to connect with everyone and it is also easy to use.',
+        link: 'https://github.com/Milan-Sharma-449/chat-app',
+        llink: 'https://chat-web-app-d4c52.web.app/',
+    }
+];
